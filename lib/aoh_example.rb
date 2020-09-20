a4d7fb25127a0f12da01bcbb58f3deadcfab879f
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-20 21:23:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,7 +14,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  
+  infinite_jest = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   
 end
 
